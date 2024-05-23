61343cd5735576b0077c444f255861f76b01d667
isaac <isaacikapel339@gmail.com>
2024-05-23 15:47:53 +0300

    changed files from jsx to js and from scss to css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Dashboard from "./components/Dashboard";
+import Sidebar from "./components/Sidebar";
+import "./scss/style.css";
+
 
 function App() {
   return (
     <div className="App">
-     
+      <Sidebar />
+      <Dashboard />
     </div>
   );
 }
